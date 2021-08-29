c3ed8264494fd030aaec27561564ee4849d41b7a
MerrickO <maedrae@hotmail.co.za>
2021-08-29 18:26:46 +0200

    add aos script

--- powerStarphp/partials/footer.php
+++ powerStarphp/partials/footer.php
@@ -1,6 +1,11 @@
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
     crossorigin="anonymous"></script>
+      <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
 <script src="/public/js/script.js" type="text/javascript"></script>
 <footer class="theme-body-space">
     <div class="container">
