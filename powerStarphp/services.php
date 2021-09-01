45a5b22706e869646a6cabc4ed89b687a5cf23ad
MerrickO <maedrae@hotmail.co.za>
2021-09-01 22:36:12 +0200

    restructure service page

--- powerStarphp/services.php
+++ powerStarphp/services.php
@@ -7,41 +7,35 @@
                 <h1 class="display-3 theme-bg mb-0 theme-header-space"><b>SERVICES</b></h1>
             </header>
             <div class="theme-grey">
-                <div class="container">
-                    <div class="row align-items-center theme-body-space">
-                        <div class="col-xl-6">
-                            <div class="card bg-dark text-white">
-                                <img src="/public/imgs/site-images/powerstar-truck-main.jpg" class="card-img" alt="...">
-                                <div class="card-img-overlay">
-                                    <h5 class="card-title">Types of vehicle that can be repaired in our workshop</h5>
-                                    <ul class="card-text ul-bullet">
-                                        <li>Passenger / LDV</li>
-                                        <li>Medium Commercial (up to 5 ton) • Heavy Commercial (5 tons +)</li>
-                                        <li>Yellow Fleet</li>
-                                        <li>Armoured Vehicles</li>
-                                        <li>Trailers</li>
-                                        <li>Fork Lifts</li>
-                                        <li>Ambulance</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-6">
-                            <div class="container text-center">
-                                <h4 class="theme-text-color">Our Competitive Edge</h4>
-                                <ul class="ul-bullet">
-                                    <li>Service driven</li>
-                                    <li>Full Truck maintenance – bumper to bumper</li>
-                                    <li>In-house refurbishment</li>
-                                    <li>Provide specialised build to your application – you design it, we build it
-                                    </li>
-                                    <li>Excellent after-sales service</li>
-                                    <li>24-hour on-call service</li>
-                                    <li>Roadside Assistance</li>
-                                    <li>24/ 7 parts availability</li>
-                                </ul>
-                            </div>
 
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-6 service-img theme-body-space theme-text-color3">
+                            <!-- <img src="/public/imgs/site-images/powerstar-truck-main.jpg" class="card-img" alt="..."> -->
+                                <h4>Types of vehicle that can be repaired in our workshop</h4>
+                                <ul class="ul-bullet">
+                                    <li>Passenger / LDV</li>
+                                    <li>Medium Commercial (up to 5 ton) • Heavy Commercial (5 tons +)</li>
+                                    <li>Yellow Fleet</li>
+                                    <li>Armoured Vehicles</li>
+                                    <li>Trailers</li>
+                                    <li>Fork Lifts</li>
+                                    <li>Ambulance</li>
+                                </ul>
+                        </div>
+                        <div class="col-xl-6 theme-body-space">
+                            <h4 class="theme-text-color">Our Competitive Edge</h4>
+                            <ul class="ul-bullet">
+                                <li>Service driven</li>
+                                <li>Full Truck maintenance – bumper to bumper</li>
+                                <li>In-house refurbishment</li>
+                                <li>Provide specialised build to your application – you design it, we build it
+                                </li>
+                                <li>Excellent after-sales service</li>
+                                <li>24-hour on-call service</li>
+                                <li>Roadside Assistance</li>
+                                <li>24/ 7 parts availability</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
@@ -50,7 +44,7 @@
                 <div class="container">
                     <h4 class="text-center theme-text-color">About Us</h4>
                     <h1 class="text-center theme-text-color"><b>Other Services Offered</b></h1>
-                    <div class="row p-5">
+                    <div class="row p-sm-5">
                         <div class="col-xl-6">
                             <ul class="ul-bullet">
                                 <li>Vehicle services – light & heavy-duty vehicles</li>
