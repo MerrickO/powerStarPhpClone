0db047e62bf44594fd1d81624ec7a02302d4ad20
MerrickO <maedrae@hotmail.co.za>
2021-09-07 22:32:13 +0200

    re-structure logic

--- powerStarphp/mail-handler.php
+++ powerStarphp/mail-handler.php
@@ -1,41 +1,24 @@
 <?php
-// get form values
-    $name = $_POST['name'];
-    $surname = $_POST['surname'];
-    $tel = $_POST['telNo'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $page_title = $_POST['pageName'];
+if(isset($_POST['submit'])) {
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $surname = filter_var($_POST['surname'], FILTER_SANITIZE_STRING);;
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $webpage = $_POST['pageName'];
+    $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);;
 
-    // powersc admin email
-    $email = "admin@powerstarsc.com";
-    $test_email = "programmerscodes@gmail.com"; 
-    // echo("<h1>Results</h1>");
-    // echo("Hello {$message}, {$tel}, {$email},{$page_title}");
-if($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(!empty($name) && !empty($email)) {
+        $mailTo = "";
+        $headers = "From: noreply@ai.net";
+        $subject = 'This is a test email';
+        $txt = "This email was sent from the ".$webpage." webpage. \n\n Name: " .$name. "\n Surname: ".$surname."\n Email: ".$email."\n Message:" .$message;
+        mail($mailTo, $subject, $txt, "From: noreply@ai.net");
     
-    echo("Hello WOrld");
-    $to = "email@example.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $name . " " . $surname . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
-
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail("merrickowenmorris@gmail.com",$subject,$message, $headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
+        echo("\n If you see this the email sent");
+    } else {
+        // header("refresh:5;url=contact.php");
+    }
 
 } else {
-    // error if no values are received
+    echo("Could not submit the form");
 }
-
-if(isset($_POST['submit'])){
-    
-    }
 ?>
