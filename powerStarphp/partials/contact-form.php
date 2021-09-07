a629309bcbb9d31c18142dcffeab4dba5f1eb653
MerrickO <maedrae@hotmail.co.za>
2021-09-07 22:32:44 +0200

    rename elements, add form elements

--- powerStarphp/partials/contact-form.php
+++ powerStarphp/partials/contact-form.php
@@ -1,18 +1,22 @@
-<div class="col-12">
-    <label for="name">Name:</label>
-    <input class="form-control pb-2" type="text" name="name"/>
-    <label for="surname pb-2">Surname</label>
-    <input class="form-control" type="text" name="surname"/>
-</div>
-<div class="col-12">
-    <label for="telNo">Telephone:</label>
-    <input class="form-control pb-2" type="telNo" name="telNo" pattern="[0-9]{10}"/>
-    <label for="email"><span>*</span>Email:</label>
-    <input class="form-control pb-2" type="email" name="email" required/>
-    <input type="hidden" value="<?php $title ?>" name="pageName"/>
-</div>
-<div class="col-12 pb-5">
-    <label for="message">Message:</label>
-    <textarea class="form-control" name="message"></textarea>
-</div>
-<button class="btn btn-lg theme-btn">Submit</button>
+<form action="./mail-handler.php" method="POST">
+    <div class="row">
+        <div class="col-12">
+            <label for="name">Name:</label>
+            <input class="form-control pb-2" type="text" name="name"/>
+            <label for="surname pb-2">Surname</label>
+            <input class="form-control" type="text" name="surname"/>
+        </div>
+        <div class="col-12">
+            <label for="telNo">Contact Number(Cell No.):</label>
+            <input class="form-control pb-2" type="telNo" name="contact" pattern="[0-9]{10}"/>
+            <label for="email"><span>*</span>Email:</label>
+            <input class="form-control pb-2" type="email" name="email" required/>
+            <input type="hidden" value="<?php $title ?>" name="pageName"/>
+        </div>
+        <div class="col-12 pb-5">
+            <label for="message">Message:</label>
+            <textarea class="form-control" name="message"></textarea>
+        </div>
+        <button type="submit" name="submit" class="btn btn-lg theme-btn">Submit</button>
+    </div>
+</form>
