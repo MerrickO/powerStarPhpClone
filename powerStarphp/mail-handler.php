167e5f2646de2827efc38d8073e2ad44049fffcb
MerrickO <maedrae@hotmail.co.za>
2021-09-09 23:13:47 +0200

    Restructure mail handler

--- powerStarphp/mail-handler.php
+++ powerStarphp/mail-handler.php
@@ -1,4 +1,5 @@
 <?php
+
 if(isset($_POST['submit'])) {
     $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
     $surname = filter_var($_POST['surname'], FILTER_SANITIZE_STRING);;
@@ -11,14 +12,24 @@ if(isset($_POST['submit'])) {
         $headers = "From: noreply@ai.net";
         $subject = 'This is a test email';
         $txt = "This email was sent from the ".$webpage." webpage. \n\n Name: " .$name. "\n Surname: ".$surname."\n Email: ".$email."\n Message:" .$message;
-        mail($mailTo, $subject, $txt, "From: noreply@ai.net");
-    
-        echo("\n If you see this the email sent");
+        $send_mail = mail($mailTo, $subject, $txt, "From: noreply@ai.net");
+        
+        if($send_mail == true) {
+            $heading = "Success!!!";
+            $status = "We will be in contact with you! :)";
+            
+        } else {
+            $heading = "Something Went Wrong";
+            $status = "Oops looks like there was a problem. Please do try once more.";
+        }
+
+        header("location: ./status_page.php");
     } else {
-        // header("refresh:5;url=contact.php");
+        header("location: ./contact.php");
     }
 
 } else {
-    echo("Could not submit the form");
+    $heading = "Internal Server Error";
+    $status = "You will be redirected";
 }
 ?>
