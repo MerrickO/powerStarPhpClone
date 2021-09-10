2589ff2269697c45f4d699c3c92af510b85b95d1
MerrickO <maedrae@hotmail.co.za>
2021-09-11 00:04:34 +0200

    change email addresses and remove white space

--- powerStarphp/contact.php
+++ powerStarphp/contact.php
@@ -22,85 +22,95 @@
                         <p class="m-0"><strong>Khetha Dludla</strong></p>
                         <p class="m-0">Sales & Marketing</p>
                         <p class="m-0">
-                            <a href="mailto:sale@ai.com"
-                                data-link="mailto:sales@ai.com">sales@ai.com</a>
+                            <a href="mailto:sale@alaysa.net"
+                                data-link="mailto:sales@alaysa.net">sales@alaysa.net</a>
                         </p>
                         <p>082 870 4362 / 035 772 1739</p>
                         <p class="m-0"><strong>Sharon</strong></p>
                         <p class="m-0">Finance</p>
                         <p>
-                            <a href="mailto:finance@ai.com"
-                                data-link="mailto:finance@ai.com">finance@ai.com</a>
+                            <a href="mailto:finance@alaysa.net"
+                                data-link="mailto:finance@alaysa.net">finance@alaysa.net</a>
                         </p>
 
                         <p class="m-0"><strong>Reuel</strong></p>
                         <p class="mb-0">Parts</p>
                         <p>
-                            <a href="mailto:parts@ai.com"
-                                data-link="mailto:parts@ai.com">parts@ai.com</a>
+                            <a href="mailto:parts@alaysa.net"
+                                data-link="mailto:parts@alaysa.net">parts@alaysa.net</a>
                         </p>
 
                         <p class="m-0"><strong>Ritesh</strong></p>
                         <p class="mb-0">Parts</p>
                         <p>
-                            <a href="mailto:workshop@ai.com"
-                                data-link="mailto:workshop@ai.com">workshop@ai.com</a>
+                            <a href="mailto:workshop@alaysa.net"
+                                data-link="mailto:workshop@alaysa.net">workshop@alaysa.net</a>
                         </p>
                     </div>
                     <div>
                         <p>Unit B8C Magazulu Park C/o R34 to Melmoth & D254 Empangeni, 3880</p>
                         <p class="mt-0"> Email:
-                            <a data-link="mailto:sales@ai.com"
-                                href="mailto:sales@ai.com">sales@ai.com</a>
+                            <a data-link="mailto:sales@alaysa.net"
+                                href="mailto:sales@alaysa.net">sales@alaysa.net</a>
                         </p>
                     </div>
 
-                    <div class="mt-5">
-                        <p class="text-center"><strong>Registered with:</strong></p>
-                        <div class="row">
-                            <div class="col">
-                                <img class="img-fluid" src="/public/imgs/site-images/logo-avis.png" alt="">
+                    <div class="container">
+                        
+                        <div class="mt-5 pb-4">
+                            <p class="text-center"><strong>Registered with:</strong></p>
+                            <div class="row align-items-center">
+                                <div class="col">
+                                    <img class="img-fluid" src="/public/imgs/site-images/logo-avis.png" alt="">
+                                </div>
+                                <div class="col">
+                                    <img class="img-fluid" src="/public/imgs/site-images/logo-eqstra.jpg" alt="">
+                                </div>
+                                <div class="col">
+                                    <img class="img-fluid" src="/public/imgs/site-images/logo-fleetafrica.png" alt="">
+                                </div>
+                                <div class="col">
+                                    <img class="img-fluid" src="/public/imgs/site-images/logo-kcs.png" alt="">
+                                </div>
                             </div>
-                            <div class="col">
-                                <img class="img-fluid" src="/public/imgs/site-images/logo-eqstra.jpg" alt="">
-                            </div>
-                            <div class="col">
-                                <img class="img-fluid" src="/public/imgs/site-images/logo-fleetafrica.png" alt="">
-                            </div>
-                            <div class="col">
-                                <img class="img-fluid" src="/public/imgs/site-images/logo-kcs.png" alt="">
-                            </div>
-                            <div class="col">
-                                <img class="img-fluid" src="/public/imgs/site-images/logo-umhlath.png" alt="">
-                            </div>
-                            <div class="col">
-                                <img class="img-fluid" src="/public/imgs/site-images/logo-wesbank.png" alt="">
-                            </div>
-                            <div class="col">
-                                <img class="img-fluid" src="/public/imgs/site-images/standard-bank.png" alt="">
-                            </div>
-                            <div class="col">
-                                <img class="img-fluid" src="/public/imgs/site-images/eskom-logo.png"
-                                    alt="">
+                            <div class="row align-items-center">
+                                <div class="col">
+                                    <img class="img-fluid" src="/public/imgs/site-images/logo-umhlath.png" alt="">
+                                </div>
+                                <div class="col">
+                                    <img class="img-fluid" src="/public/imgs/site-images/logo-wesbank.png" alt="">
+                                </div>
+                                <div class="col">
+                                    <img class="img-fluid" src="/public/imgs/site-images/standard-bank.png" alt="">
+                                </div>
+                                <div class="col">
+                                    <img class="img-fluid eskom-resize" src="/public/imgs/site-images/eskom-logo.png"
+                                        alt="" />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <!-- Contact Form and Maps -->
-                    <div class="p-5 row">
-                        <div class="col-xl-6">
-                            <p>Google Maps</p>
-                        </div>
-                        <div class="col-xl-6">
-                            <?php include './partials/contact-form.php'; ?>
+                        <!-- Contact Form and Maps -->
+                        <div class="p-5 row">
+                            <div class="col-xl-6 pb-5 pb-md-0">
+                                <h3 class="pb-4">Our Location</h3>
+                                <iframe class="ratio ratio-1x1" height="400" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3589.2103406829897!2d28.16087791502373!3d-25.895454983576222!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e95659eeeb6ef8f%3A0x6bcd1e0672073ab7!2s103%20Park%20Ave%2C%20Rooihuiskraal%2C%20Centurion%2C%200154!5e0!3m2!1sen!2sza!4v1631308184612!5m2!1sen!2sza" allowfullscreen="" loading="lazy"></iframe>
+                            </div>
+                            <div class="col-xl-6">
+                                <h3>Contact Us!</h3>
+                                <?php include './partials/contact-form.php'; ?>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="row text-center">
-                    <div class="col pb-5">
-                        <p>Download Our Company Profile</p>
-                        <a class="btn btn-lg theme-btn" href="/public//imgs/downloads/pstar-company-profile-sm.pdf" download>Download</a>
+                <div class="container">
+                    <div class="row text-center">
+                        <div class="col pb-5">
+                            <p>Download Our Company Profile</p>
+                            <a class="btn btn-lg theme-btn" href="/public//imgs/downloads/pstar-company-profile-sm.pdf" download>Download</a>
+                        </div>
                     </div>
                 </div>
+
             <?php include './partials/footer.php'; ?>
     </body>
 
