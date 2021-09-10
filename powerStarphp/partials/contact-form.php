150a33e3284b367d8349ee4ab94536227dc1dba3
MerrickO <maedrae@hotmail.co.za>
2021-09-11 00:04:54 +0200

    add spacing

--- powerStarphp/partials/contact-form.php
+++ powerStarphp/partials/contact-form.php
@@ -3,20 +3,27 @@
         <div class="col-12">
             <label for="name">Name:</label>
             <input class="form-control pb-2" type="text" name="name"/>
+            <br/>
             <label for="surname pb-2">Surname</label>
             <input class="form-control" type="text" name="surname"/>
+            <br/>
         </div>
         <div class="col-12">
             <label for="telNo">Contact Number(Cell No.):</label>
             <input class="form-control pb-2" type="telNo" name="contact" pattern="[0-9]{10}"/>
+            <br/>
             <label for="email"><span>*</span>Email:</label>
-            <input class="form-control pb-2" type="email" name="email" required/>
+            <input class="form-control pb-2" type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required/>
             <input type="hidden" value="<?php $title ?>" name="pageName"/>
         </div>
         <div class="col-12 pb-5">
+            <br/>
             <label for="message">Message:</label>
             <textarea class="form-control" name="message"></textarea>
         </div>
-        <button type="submit" name="submit" class="btn btn-lg theme-btn">Submit</button>
+        <button 
+        data-sitekey="reCAPTCHA_site_key" 
+        data-callback='onSubmit' 
+        data-action='submit' type="submit" name="submit" class="g-recaptcha btn btn-lg theme-btn">Submit</button>
     </div>
 </form>
