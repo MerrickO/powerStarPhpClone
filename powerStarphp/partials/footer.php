7c58680f4c620855f365cfc738ab39911be4b0e8
MerrickO <maedrae@hotmail.co.za>
2021-09-11 00:05:11 +0200

    add script for capcha

--- powerStarphp/partials/footer.php
+++ powerStarphp/partials/footer.php
@@ -2,9 +2,13 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
     crossorigin="anonymous"></script>
+     <script src="https://www.google.com/recaptcha/api.js"></script>
       <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
   <script>
     AOS.init();
+    function onSubmit(token) {
+     document.getElementById("demo-form").submit();
+   }
   </script>
 <script src="/public/js/script.js" type="text/javascript"></script>
 <footer class="theme-body-space">
