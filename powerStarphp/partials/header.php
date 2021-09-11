2dd4e5fb9fbbf8e312f571abfb9864e9bca25e2f
MerrickO <maedrae@hotmail.co.za>
2021-09-11 10:34:41 +0200

    remove light gallery

--- powerStarphp/partials/header.php
+++ powerStarphp/partials/header.php
@@ -16,7 +16,6 @@
     <title><?php if($title != "") { echo $title;} else{ echo "Powerstar Service Center";}?></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.1.8/css/lightgallery-bundle.min.css" integrity="sha512-7xAjdVc23n18sYEAZKa79271IRUS1XzylIyJ8feyXvjrbbr4oId92993EiGTAP63Ob6KplRnmbNmBOg6bpLjYw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="/public/css/style.css"/>
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 </head>
